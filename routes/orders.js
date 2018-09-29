c42a49a074a2d3c51a07b0dfd3a709f63e2e0f09
谢刘铎 <959459496@qq.com>
2018-09-29 14:43:43 +0800

    与 swagger 相互搭配的 Joi 插件的用法 和 接口校验 ok

--- routes/orders.js
+++ routes/orders.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const GROUP_NAME = 'orders';
 
 module.exports = [{
@@ -11,6 +12,19 @@ module.exports = [{
             config: {
                 tags: ['api', GROUP_NAME],
                 description: '创建订单',
+                validate: {
+                    payload: {
+                        goodsList: Joi.array().items(
+                            Joi.object().keys({
+                                goods_id: Joi.number().integer(),
+                                count: Joi.number().integer()
+                            })
+                        )
+                    },
+                    headers: Joi.object({
+                        authorization: Joi.string().required()
+                    }).unknown()
+                }
             },
     },
     {
@@ -23,6 +37,11 @@ module.exports = [{
             config: {
                 tags: ['api', GROUP_NAME],
                 description: '支付某条订单',
+                validate: {
+                    params: {
+                        orderId: Joi.string().required(),
+                    }
+                }
             },
     },
 ];
--- routes/shops.js
+++ routes/shops.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const GROUP_NAME = 'shops';
 
 function configObj(GROUP_NAME, des) {
@@ -13,9 +14,24 @@ module.exports = [{
         config: {
             tags: ['api', GROUP_NAME],
             description: '获取店铺列表',
+            validate: {
+                query: {
+                    limit: Joi.number().integer().min(1).default(10).description('每页的条数目'),
+                    page: Joi.number().integer().min(1).default(1).description('页码数')
+                }
+            }
         },
         handler: async (request, reply) => {
-                reply();
+                //console.log('GROUP_NAME', GROUP_NAME);
+                console.log('-----------------------------------------------------------------------------------');
+                console.log('request.headers', request.headers);
+                console.log('--------------------');
+                console.log('request.url', request.url);
+                console.log('--------------------');
+                console.log('request.info', request.info);
+                console.log('GET shops');
+
+                reply(GROUP_NAME);
             },
 
     },
